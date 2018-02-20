28bb7575afac64bb9d311e47ec21910fd6b14126
Kevin Tran <kevin2005tran@gmail.com>
2018-02-19 17:21:12 -0800

    adding easy algorithms

---
+++ Algorithms/Easy/Easy_Algorithms.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+ JEWELS AND STONES
+ You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+
+Input: J = "z", S = "ZZ"
+Output: 0
+ */
+
+/*
+func numJewelsInStones(_ J: String, _ S: String) -> Int {
+    var outPut: Int = 0
+    
+    for jem in J {
+        for stone in S {
+            if jem == stone {
+                outPut += 1
+            }
+        }
+    }
+    
+    return outPut
+}
+ */
+
+
+
